0d7e378fa84b9c02724f47ded1cc94037740ac85
irosen419 <irosen419@gmail.com>
2024-05-28 11:35:05 -0500

    render employees from fetch request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import CalendarContainer from './Calendar/CalendarContainer'
 import './App.css';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: <div>Please login</div>
+    },
+    {
+      path: "/signup",
+      element: <div>Please sign up</div>
+    },
+    {
+      path: "/calendar",
+      element: <CalendarContainer />
+    },
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
+      <header className='App-header'>
         <h1 className='App-name'>Coffee Shop Scheduler</h1>
       </header>
+      <div className='App-container'>
+        <RouterProvider router={router} />
+      </div>
+      <footer className='App-footer'>
+
+      </footer>
     </div>
   );
 }
